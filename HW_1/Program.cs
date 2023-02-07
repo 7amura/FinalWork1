b5633e461d9f3f1b434f7de1d39c9a85aca84b72
Daria <dasha.shershneva.97@mail.ru>
2023-02-07 16:59:41 +0300

    NewTask

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -2,3 +2,38 @@
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
 // исключительно массивами.
+
+string[] FilterArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+string[] myArray = {"hello", "2", "world", ":-)", "1234", "123", "-2"};
+PrintArray(FilterArray(myArray));
